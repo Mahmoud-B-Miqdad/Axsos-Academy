803de508f56057c98406b136004138b0147804c0
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2026-04-11 17:25:32 +0300

    feat: complete hello world assignment covering string interpolation and variables.

---
+++ python_stack/_python/python_fundamentals/HelloWorldAssignment/hello_world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+name = "Mahmoud" 
+print("Hello", name)        
+print("Hello " + name)      
+
+num = 7
+print("Hello", num, "!")     
+
+print("Hello " + str(num) + "!") 
+
+fave_food1 = "Shwarma"
+fave_food2 = "pizza"
+
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+
+print(f"I love to eat {fave_food1} and {fave_food2}.")
