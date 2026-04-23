2324171a97418087d73769b96dba231e983c9603
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2026-04-23 12:26:57 +0300

    feat: implement playground assignment with jinja2 templating

---
+++ python_stack/flask/flask_fundamentals/playground/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/play')
+@app.route('/play/<int:x>')
+@app.route('/play/<int:x>/<color>')
+def play(x=3, color="lightblue"):
+    return render_template("index.html", num=x, color=color)
+
+if __name__ == "__main__":
+    app.run(debug=True)
